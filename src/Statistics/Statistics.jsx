596e579f81579ac4ada9632b00e03f1e73312987
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-16 22:22:31 +0200

    2

--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,10 +1,10 @@
 import './Statistics.css'
-import stats from './data.json';
+// import stats from './data.json';
 export const Statistics = () => 
 {
     return(
         <>
-        <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, magni fugiat quaerat incidunt blanditiis laboriosam facere iure officiis, sunt sit voluptatum quos itaque molestiae? Minus aperiam incidunt inventore odit ullam!</h3>
+        <h3>LLLorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, magni fugiat quaerat incidunt blanditiis laboriosam facere iure officiis, sunt sit voluptatum quos itaque molestiae? Minus aperiam incidunt inventore odit ullam!</h3>
         </>
     )
 }
