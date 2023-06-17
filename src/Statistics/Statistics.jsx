0fc5431d815d69935bbd349005fb8adca4371bee
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-17 22:46:26 +0200

    2

--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,16 +1,33 @@
 import './Statistics.css'
 import stats from './data.json';
+
 export const Statistics = () => 
 {
-    
       return(
     <>  
-    <div className='statistic'>
-      <div className='statistic-wrapper'></div>
+    <section className='statistic'>
+      <h2 className='statistic-wrapper'>Upload stats</h2>
         <ul className='statistic-ul'>
-              {stats.map(stat => (<li className="statistic-li" key={stat.id}>{stat.label} <br></br>{stat.percentage}%</li>))}
+              {stats.map(stat => (
+              <li className='statistic-li' style={col} key={stat.id}>
+                <span>{stat.label}</span>
+                <span>{stat.percentage}%</span>
+                </li>))}
         </ul>
-    </div>
+    </section>
     </>
     )
 }
+
+
+function Hex() {
+  return Math.round(Math.random() * 256)
+    .toString(16)
+    .padStart(2, "0");
+}
+function Hex3() {
+  return `#${Hex()}${Hex()}${Hex()}`;
+}
+const hex31 = Hex3()
+const col = {
+  backgroundColor: hex31};
