2d3e450d874187459b6f6a57844ee63bdfd6ccff
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-18 14:05:50 +0200

    4

---
+++ src/TransactionHistory/Th.jsx
@@ -0,0 +1,13 @@
+import th from './transactions.json'
+import './Th.css'
+
+console.log(th)
+
+export const TransactionHistory = () => 
+{
+    return (
+        <>
+        <p>sss</p>
+        </>
+    )
+}
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import { Profile } from "Profile/Profile";
 import { Statistics } from "Statistics/Statistics";
 import { FriendListItem } from "FriendList/FriendList";
-
+import { TransactionHistory } from "TransactionHistory/Th"
 export const App = () => {
   return (
 <>    
 <Profile/>
 <Statistics/>
 <FriendListItem/>
+<TransactionHistory/>
 </>
   );
 };
