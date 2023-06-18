647ab53095e632043f5b2606d9caa0652abe36bd
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-18 02:29:07 +0200

    3

--- src/FriendList/FriendList.jsx
+++ src/FriendList/FriendList.jsx
@@ -2,21 +2,18 @@ import friendlist from './friends.json'
 import "./Friendlist.css"
 
 export const FriendListItem = () => {
-
     return (
-      
     <ul style={{listStyle: 'none'}}>
   
           {friendlist.map(({ avatar, name, isOnline, id, }) => (
              <>
-             <li className='friend' key={id}> 
-            {isOnline ? <span className='friend-led' style={{backgroundColor: 'red'}}></span> : <span className="friend-led" style={{backgroundColor: 'green'}}></span> }
-             <img src={avatar} style={{width: 48}} alt="User avatar"></img>
-             <p className='friend-avatar'>{name}</p>
-             </li>
+                <li className='friend' key={id}> 
+                    {isOnline ? <span className='friend-led' style={{backgroundColor: 'red'}}></span> : <span className="friend-led" style={{backgroundColor: 'green'}}></span> }
+                    <img src={avatar} style={{width: 48}} alt="User avatar"></img>
+                    <p className='friend-avatar'>{name}</p>
+                </li>
             </>
           ))}
-
     </ul>
     );
   }
