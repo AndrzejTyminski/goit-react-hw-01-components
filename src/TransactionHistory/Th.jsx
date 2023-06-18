38aac17f08257df8a89e2eb5a7702daba03304b7
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-18 15:02:31 +0200

    4

--- src/TransactionHistory/Th.jsx
+++ src/TransactionHistory/Th.jsx
@@ -1,13 +1,29 @@
 import th from './transactions.json'
 import './Th.css'
-
-console.log(th)
-
-export const TransactionHistory = () => 
+export const TransactionHistory = (  ) => 
 {
     return (
         <>
-        <p>sss</p>
+        <table className='transaction-history'> 
+            <thead>
+                <tr >
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
+                </tr>
+                </thead>
+                <tbody >
+            {th.map(({ id, type, amount, currency }) => ( 
+        <>
+        <tr key={id}>
+            <td className='td-type'>{type}</td>
+            <td className='td-type'>{amount}</td>
+            <td className='td-type'>{currency}</td>
+        </tr>
+        </>
+            ))}
+                </tbody>
+        </table>
         </>
     )
 }
