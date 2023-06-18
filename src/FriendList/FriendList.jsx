7aabfbd971857ff9900329d843f5d516b57df7aa
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-18 13:55:50 +0200

    3

--- src/FriendList/FriendList.jsx
+++ src/FriendList/FriendList.jsx
@@ -4,15 +4,12 @@ import "./Friendlist.css"
 export const FriendListItem = () => {
     return (
     <ul style={{listStyle: 'none'}}>
-  
           {friendlist.map(({ avatar, name, isOnline, id, }) => (
-             <>
                 <li className='friend' key={id}> 
                     {isOnline ? <span className='friend-led' style={{backgroundColor: 'red'}}></span> : <span className="friend-led" style={{backgroundColor: 'green'}}></span> }
                     <img src={avatar} style={{width: 48}} alt="User avatar"></img>
                     <p className='friend-avatar'>{name}</p>
                 </li>
-            </>
           ))}
     </ul>
     );
--- src/Statistics/Statistics.jsx
+++ src/Statistics/Statistics.jsx
@@ -1,28 +1,26 @@
 import './Statistics.css'
 import stats from './data.json';
-
 export const Statistics = () => 
 {
       return(
     <>  
     <section className='statistic'>
       <h2 className='statistic-wrapper'>Upload stats</h2>
-        <ul className='statistic-ul'>
-              {stats.map(stat => (
-              <li className='statistic-li' style={col} key={stat.id}>
-                <span>{stat.label}</span>
-                <span>{stat.percentage}%</span>
-                </li>))}
-        </ul>
+       <ul className='statistic-ul'>  
+                {stats.map(({ id, label, percentage, title }) => (
+                  <li className='statistic-li' style={col} key={id}>
+                  <span>{label}</span>
+                  <span>{percentage}%</span>
+                  </li>
+                ))}
+         </ul>
     </section>
     </>
     )
 }
-
-
 function Hex() {
   return Math.round(Math.random() * 256)
-    .toString(16)
+    .toString(11)
     .padStart(2, "0");
 }
 function Hex3() {
