69be1d254be288abf24a9354e735213c7af7afa7
Andrzej Tymiński <info@andrzejtyminski.pl>
2023-06-20 00:02:47 +0200

    friend list class name

--- src/FriendList/FriendList.jsx
+++ src/FriendList/FriendList.jsx
@@ -5,7 +5,7 @@ const FriendListItem = ( {items} ) => {
     <ul style={{listStyle: 'none'}}>
           {items.map(({ avatar, name, isOnline, id, }) => (
                 <li className={css.friend} key={id}> 
-                    {isOnline ? <span className={css.friendLed} style={{backgroundColor: 'green'}}></span> : <span className="friend-led" style={{backgroundColor: 'red'}}></span> }
+                    {isOnline ? <span className={css.friendLed} style={{backgroundColor: 'green'}}></span> : <span className={css.friendLed} style={{backgroundColor: 'red'}}></span> }
                     <img src={avatar} style={{width: 48}} alt="User avatar"></img>
                     <p className={css.friendAvatar}>{name}</p>
                 </li>
